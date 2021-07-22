25d4635026b5b1cbad319e3afc7bb09ffa061ab8
Anderson Jardim <andersonjardim@gmail.com>
2021-07-22 03:09:40 -0300

    Adicionado componentes Button.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Button } from './components/Button'
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1> Hello World - Agora Vai! </h1>
-    </div>
+      <Button text="Botão 1" />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+
+    </div >
   );
 }
 
