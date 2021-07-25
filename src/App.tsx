8bba6c46659753a6fabf0ebaccff89b4f203a63a
Anderson Jardim <andersonjardim@gmail.com>
2021-07-24 21:22:22 -0300

    Fazendo o boão mostrar quantas vezes clicou
    Resumo:
    <Button />
    setCounter(counter + 1);

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div>
       <h1> Hello World - Agora Vai! </h1>
-      <Button>Clique aqui</Button>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
       <Button />
       <Button />
       <Button />
